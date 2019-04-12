39b59921c171c95f50687e8c6cc400be7bf09587
nitanmarcel <nitan.marcel@gmail.com>
2019-04-13 00:17:18 +0300

    Fix error when a user without username uses the bot

--- russianroulettebot.py
+++ russianroulettebot.py
@@ -32,7 +32,7 @@ async def start(event):
 async def roulette(event):
     builder = event.builder
     user = await bot.get_entity(event.query.user_id)
-    user_name = "@" + user.username or user.first_name
+    user_name = "@" + user.username if user.username else user.first_name
     deaths = [f"{user_name}'s guts were spilled all over the floor'",
                 f"{user_name} just got killed...",
                 f"{user_name} just got served!",
